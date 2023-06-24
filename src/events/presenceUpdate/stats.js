cdf95fac5079ecaf25a83646b2116a961b375ccf
PraditaAdiantama <aditadiantama@gmail.com>
2023-06-24 18:40:30 +0700

    refactor: move guild-id and channel-id to env

--- src/events/presenceUpdate/stats.js
+++ src/events/presenceUpdate/stats.js
@@ -1,9 +1,11 @@
+const dotenv = require('dotenv').config()
+
 module.exports = function (oldState, newState, client) {
     const member = client.guilds.cache
-        .get('744118069672017930').members.cache
+        .get(process.env.GUILD_ID).members.cache
         .filter(member => !member.user.bot).size
 
     client.channels.cache
-        .get('1121942597628338196')
-        .setName(`Member: ${member}`)
+        .get(process.env.MEMBER_STATS_CHANNEL_ID)
+        .setName(`Members: ${member}`)
 }
