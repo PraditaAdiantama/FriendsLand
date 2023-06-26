1937e648e27f7e561c743e9b23fddf0595561ccf
PraditaAdiantama <aditadiantama@gmail.com>
2023-06-26 17:58:38 +0700

    feat: auto-role fiture when level up in spesific level

--- src/events/messageCreate/give-user-xp.js
+++ src/events/messageCreate/give-user-xp.js
@@ -2,6 +2,7 @@ const { Client, Message } = require('discord.js')
 const Level = require('../../models/Level')
 const calculateLevelXp = require('../../utils/calculateLevelXp')
 const dotenv = require('dotenv').config();
+const { roles } = require('../../../config.json')
 
 function getRandomXp(min, max) {
     min = Math.ceil(min)
@@ -16,7 +17,7 @@ function getRandomXp(min, max) {
  */
 
 module.exports = async (message, client) => {
-    if(!message.inGuild() || message.author.bot) return
+    if (!message.inGuild() || message.author.bot) return
 
     const xpToGive = getRandomXp(5, 10)
 
@@ -28,10 +29,10 @@ module.exports = async (message, client) => {
     try {
         const level = await Level.findOne(query)
 
-        if(level){
+        if (level) {
             level.xp += xpToGive;
 
-            if(level.xp > calculateLevelXp(level.level)){
+            if (level.xp > calculateLevelXp(level.level)) {
                 level.xp = 0
                 level.level += 1
 
@@ -42,10 +43,17 @@ module.exports = async (message, client) => {
                 console.log(e);
                 return
             })
+
+            const rookie = message.guild.roles.cache.get(roles.rookie);
+            const veteran = message.guild.roles.cache.get(roles.veteran);
+            const sepuh = message.guild.roles.cache.get(roles.sepuh);
+            if (level.level > 10 && !message.member.roles.cache.has(roles.rookie)) message.member.roles.add(rookie);
+            else if (level.level > 30 && !message.member.roles.cache.has(roles.veteran)) message.member.roles.add(veteran);
+            else if (level.level > 50 && !message.member.roles.cache.has(roles.sepuh)) message.member.roles.add(sepuh);
         }
 
         // if(!level)
-        else{
+        else {
             const newLevel = new Level({
                 userId: message.author.id,
                 guildId: message.guild.id,
