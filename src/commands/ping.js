d2d65a1f2e2ceaaf6ea12e51a351ccb6d294e845
PraditaAdiantama <aditadiantama@gmail.com>
2023-07-02 06:48:06 +0700

    fix: double slash ping command

--- src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js')
-module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('reply with pong'),
-    run: ({ interaction }) => {
-        interaction.reply('Pong!');
-    },
-    devOnly: false,
-}
