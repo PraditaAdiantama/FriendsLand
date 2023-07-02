26145ec4891fefd7384d2416ac55071b50ce3b56
PraditaAdiantama <aditadiantama@gmail.com>
2023-07-02 21:45:45 +0700

    chore: add canvas funtion and refactor some code

--- src/commands/level.js
+++ src/commands/level.js
@@ -1,16 +1,26 @@
-const { Client, Interaction, SlashCommandBuilder } = require('discord.js')
+const { Client, InteractionResponse, SlashCommandBuilder } = require('discord.js')
 const Level = require('../models/Level')
+const levelCanvas = require('../utils/level-canvas')
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('level').setDescription("Menampilkan level"),
+    data: new SlashCommandBuilder()
+        .setName('level')
+        .setDescription("Menampilkan level")
+        .addStringOption(option =>
+            option.setName('target-user')
+                .setDescription('Mention member')
+        ),
+    /**
+     * @param {InteractionResponse} interaction
+     */
     run: async ({ interaction, client }) => {
-        if(!interaction.inGuild()){
+        if (!interaction.inGuild()) {
             interaction.reply('Command hanya bisa di jalankan di server')
             return
         }
         await interaction.deferReply()
 
-        const mentionUserId = interaction.options.get('target-user')?.value
+        const mentionUserId = interaction.options.get('target-user')?.value.replace('@', '').replace('<', '').replace('>', '')
         const targetUserId = mentionUserId || interaction.member.id
         const targetUserObj = await interaction.guild.members.fetch(targetUserId)
 
@@ -19,22 +29,13 @@ module.exports = {
             guildId: interaction.guild.id
         })
 
-        if(!fetchLevel){
+        if (!fetchLevel) {
             interaction.editReply(
                 mentionUserId ? `${targetUserObj.user.tag} Belum memiliki level sama sekali` : "Kamu belum memiliki level "
             )
             return
         }
-        let allLevels = await Level.find({guildId: interaction.guild.id}).select('-_id userId level xp')
 
-        allLevels.sort((a,b) => {
-            if(a.level === b.level){
-                return b.xp - a.xp;
-            }else{
-                return b.level - a.level;
-            }
-        })
-
-        let currentRank = allLevels.findIndex((lvl) => lvl.userId === targetUserId) + 1
+        levelCanvas(interaction, fetchLevel, targetUserObj);
     }
 }
